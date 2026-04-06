47c21710d10d42d59abbba8c6cbeffc8af1ab50d
Mohamed Maged <abomaged9990@gmail.com>
2026-04-06 22:51:26 +0200

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -233,7 +233,7 @@
 {{-- Statistics Bar --}}
 <section class="bg-navy py-10" x-data="statsCounter()" x-intersect.once="startCounting()">
     <div class="container mx-auto px-4">
-        <div class="grid grid-cols-4 gap-6 text-center">
+        <div style="display:grid; grid-template-columns:repeat(4,1fr); gap:1.5rem; text-align:center">
             @php
                 $statsData = [
                     ['value' => (int)($stats['students']     ?? 20000), 'label_ar' => 'طالب مسجل',    'label_en' => 'Registered Students',      'suffix' => '+'],
