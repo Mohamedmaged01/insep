edea9f1ace7749eee18db49ce49ee8837066125c
Mohamed Maged <abomaged9990@gmail.com>
2026-04-06 23:15:20 +0200

    update

--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Cache;
 
 class SiteSetting extends Model
 {
@@ -11,16 +10,13 @@ class SiteSetting extends Model
 
     public static function get(string $key, $default = null)
     {
-        return Cache::remember("site_setting_{$key}", 3600, function () use ($key, $default) {
-            $setting = static::where('key', $key)->first();
-            return $setting ? $setting->value : $default;
-        });
+        $setting = static::where('key', $key)->first();
+        return $setting ? $setting->value : $default;
     }
 
     public static function set(string $key, $value): void
     {
         static::updateOrCreate(['key' => $key], ['value' => $value]);
-        Cache::forget("site_setting_{$key}");
     }
 
     public static function setMany(array $data): void
